a9976188f30f3852c4322b175e63b16792c92101
Daniel-Hercules <73712933+Daniel-Hercules@users.noreply.github.com>
2020-11-02 16:00:29 +0000

    Add files via upload

---
+++ IfElse/IfElse/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Linq;
+
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Boolean PasswordEntered = false;
+            while(!(PasswordEntered)){
+
+                Console.WriteLine("Hello! Please enter your password :  ");
+                String input = Console.ReadLine();
+
+                if (input.Equals("Daniel"))
+                {
+                    Console.WriteLine("Correct Password Inserted! Welcome Back Daniel");
+                    PasswordEntered = true;
+                }
+                else if (input.Equals("Exit")) 
+                {
+                    Console.WriteLine("Exiting System \n Goodbye!");
+                    PasswordEntered = true;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect Password Inserted Please Try again");
+                }
+            }
+        }
+    }
+}
